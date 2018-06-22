7a364c356fe86bfd4696385bf23e3dbe0e13aa84
subhash.gupta <subhash.gupta@mobiloitte.in>
2018-06-22 11:26:53 +0530

    Small Chnages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	skip_before_action :authenticate_request, only: %i[login]
+	#skip_before_action :authenticate_request, only: %i[login]
 
 	def login
 		type1 = params[:email].present? ? "email" : "mobile"

d2c01bdae6ebf3ced60ddb184cfee346655dc864
subhash.gupta <subhash.gupta@mobiloitte.in>
2018-07-04 12:52:08 +0530

    Changes in social login and added cloudinary.yml file

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -51,8 +51,8 @@ class Api::V1::SessionsController < ApplicationController
 		       @user.attributes = {email: params[:user][:email], first_name: params[:user][:first_name], last_name: params[:user][:last_name], :remote_image_url=> params[:user][:image]}
 		       @user.save
 		       @user.reload
-		       @user.social_logins.find_or_create_by(provider_id: params[:user][:provider_id], provider: params[:user][:provider])
-			   return render json: {responseCode: 200, responseMessage: "Login successfully." ,access_token: @user.access_token, first_name: @user&.first_name, last_name: @user.last_name, image: @user&.image&.url}
+		       @social = @user.social_logins.find_or_create_by(provider_id: params[:user][:provider_id], provider: params[:user][:provider])
+			   return render json: {responseCode: 200, responseMessage: "Login successfully." ,access_token: @user.access_token, first_name: @user&.first_name, last_name: @user.last_name, image: @user&.image&.url,provider_id: @social&.provider_id}
 			end	 		
 	  	rescue Exception => e
 	    	return render json: {responseCode: 500, responseMessage: "Something went wrong try again later."}  	

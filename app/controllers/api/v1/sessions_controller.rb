4b6f7b9de293482e1289e355810e3aa354ee83ea
subhash.gupta <subhash.gupta@mobiloitte.in>
2018-07-04 13:07:57 +0530

    Small Chnages

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -50,7 +50,8 @@ class Api::V1::SessionsController < ApplicationController
 			if @user.present?
 		       @user.attributes = {email: params[:user][:email], first_name: params[:user][:first_name], last_name: params[:user][:last_name], :remote_image_url=> params[:user][:image]}
 		       @user.save
-		       @user.reload
+		       # @user.reload
+		       @user = User.find_by(id: @user.id)
 		       @social = @user.social_logins.find_or_create_by(provider_id: params[:user][:provider_id], provider: params[:user][:provider])
 			   return render json: {responseCode: 200, responseMessage: "Login successfully." ,access_token: @user.access_token, first_name: @user&.first_name, last_name: @user.last_name, image: @user&.image&.url,provider_id: @social&.provider_id}
 			end	 		

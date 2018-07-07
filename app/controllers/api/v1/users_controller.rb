c8ed2903840ececceb27ba561ebe4e93af0d9f86
aryan.arora <aryan.arora@mobiloitte.in>
2018-07-07 10:18:26 +0530

    end time key added in credit api.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::UsersController < ApplicationController
 			@transaction = Transaction.create(transaction_id: "", status: true, amount: @package&.package_value   )
 			@api_current_user.billings.new(method_of_payment: "Credit",session_id: @api_current_user.sessions.last.id, package_id: @package.id,
 				usage_start_ts: DateTime.now,usage_end_ts: DateTime.now + @package&.package_time.minutes, transaction_id: @transaction.id).save(validate: false)
-			return render json: {responseCode: 200, responseMessage: "Your credit applied.", remaining_credit: @remaining_credit}
+			return render json: {responseCode: 200, responseMessage: "Your credit applied.", remaining_credit: @remaining_credit, end_time: @api_current_user&.billings&.last&.usage_end_ts&.to_i || ""}
 		else
 			return render json: {responseCode: 500, responseMessage: "Your credit is not enough."}
 		end
@@ -22,7 +22,7 @@ class Api::V1::UsersController < ApplicationController
 				@billing = @api_current_user.billings.new(method_of_payment: "Card",session_id: @api_current_user.sessions.last.id, package_id: @package.id,
 				usage_start_ts: DateTime.now,usage_end_ts: DateTime.now+@package&.package_time.minutes,transaction_id: @transaction.id )
 				if @billing.save
-					return render json: {responseCode: 200, responseMessage: "Transaction successful."}
+					return render json: {responseCode: 200, responseMessage: "Transaction successful.", end_time: @api_current_user&.billings&.last&.usage_end_ts&.to_i || ""}
 				else
 					return render json: {responseCode: 200, responseMessage: "Something went wrong."}
 				end

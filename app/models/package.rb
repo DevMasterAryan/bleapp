2a6af4ea69d4871ccadf2b4bd90ac29c28bd2029
aryan.arora <aryan.arora@mobiloitte.in>
2018-07-12 20:29:31 +0530

    field added in package model.

--- app/models/package.rb
+++ app/models/package.rb
@@ -4,5 +4,7 @@ class Package
   field :package_id, type: Integer
   field :package_time, type: Integer
   field :package_value, type: Float
+  field :package_gst, type: Float
+  field :package_final, type: Float
   has_many :billings, dependent: :destroy
 end

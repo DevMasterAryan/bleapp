42f9dedd8888a861157470cbaf248cd31527e670
aryan.arora <aryan.arora@mobiloitte.in>
2018-07-13 15:02:34 +0530

    changes in device controller

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::DevicesController < ApplicationController
 		@device = Device.any_of({mac_address: params[:qr_code]},{device: params[:qr_code]}).first
 		# @device = Device.find_by(device: params[:qr_code]) || Device.find_by(mac_address: params[:qr_code])
 		if @device.present?
-			@device_detail = {id: @device&.id&.as_json["$oid"] || "", bt_id: @device&.bluetooth_id || "", stolen_status: @device&.stolen || false, identifier: @device&.identifier || "", mac_address: @device&.mac_address || "", device_status: @device&.device_status, site_display_name: @device&.site_display_name } 
+			@device_detail = {id: @device&.id&.as_json["$oid"] || "", bt_id: @device&.bluetooth_id || "", stolen_status: @device&.stolen || false, identifier: @device&.identifier || "", mac_address: @device&.mac_address || "", device_status: @device&.device_status, site_display_name: @device&.site_display_name, site_name: @device&.site_id? ? @device&.location&.name || "" } 
 			begin
-				@session = Session.create(user_id: @api_current_user.id, device_id: @device.id, site_id: @device&.location.id, device_batterry_start: params["device_battery_start"])
+				@session = Session.create(user_id: @api_current_user.id, device_id: @device.id, site_id: @device&.location.id)
 			  	return render json: {responseCode: 200, device_detail: @device_detail, session_id: @session&.id&.as_json["$oid"] || ""}	
 			rescue Exception => e
 				return render json: {responseCode: 500, device_detail: e}	
@@ -66,7 +66,7 @@ class Api::V1::DevicesController < ApplicationController
      if @device.present?
        @session = @session.update(device_battery_start: params["device_status"])
        # @device.update(device_status: params["device_status"])
-       @device_detail = {id: @device&.id&.as_json["$oid"] || "", bt_id: @device&.bluetooth_id || "", stolen_status: @device&.stolen || false, identifier: @device&.identifier || "", mac_address: @device&.mac_address || "", device_status: @session&.device_battery_start,site_display_name: @device&.site_display_name  } 
+       @device_detail = {id: @device&.id&.as_json["$oid"] || "", bt_id: @device&.bluetooth_id || "", stolen_status: @device&.stolen || false, identifier: @device&.identifier || "", mac_address: @device&.mac_address || "", device_status: @session&.device_battery_start,site_display_name: @device&.site_display_name, site_name: @device&.site_id? ? @device&.location&.name || ""  } 
        return render json: { responseCode: 200, responseMessage: "Device status saved successfully.",device_detail: @device_detail }
      else
      	return render json: {responseCode: 500, responseMessage: "Device not found."}

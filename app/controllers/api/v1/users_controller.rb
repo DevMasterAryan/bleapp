69e5ddfc6b25efd5b7259eadcc23a9e54fb9740c
aryan.arora <aryan.arora@mobiloitte.in>
2018-07-30 15:19:35 +0530

    change in merchant id and key

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -73,8 +73,8 @@ class Api::V1::UsersController < ApplicationController
    
 
     def checksum
-        paramList = Hash.new
-	    paramList["MID"] = "mobilo96691880612413"
+     paramList = Hash.new
+	    paramList["MID"] = "Wavedi27436137685521"
 	    paramList["ORDER_ID"] = "#{Time.now.to_i.to_s}"
 	    paramList["CUST_ID"] = @api_current_user.id&.as_json["$oid"]
 	    paramList["INDUSTRY_TYPE_ID"] = "Retail"
@@ -85,6 +85,19 @@ class Api::V1::UsersController < ApplicationController
 	    paramList["WEBSITE"] = "wavedio.herkuapp.com"
 	    @paramList=paramList
         @checksum_hash=generate_checksum()
+        
+     #    paramList = Hash.new
+	    # paramList["MID"] = "mobilo96691880612413"
+	    # paramList["ORDER_ID"] = "#{Time.now.to_i.to_s}"
+	    # paramList["CUST_ID"] = @api_current_user.id&.as_json["$oid"]
+	    # paramList["INDUSTRY_TYPE_ID"] = "Retail"
+	    # paramList["CHANNEL_ID"] = "WAP"
+	    # paramList["TXN_AMOUNT"] = "1"
+	    # paramList["MSISDN"] = "+919997217401"
+	    # paramList["EMAIL"] = "gunjackaryan@gmail.com"
+	    # paramList["WEBSITE"] = "wavedio.herkuapp.com"
+	    # @paramList=paramList
+     #    @checksum_hash=generate_checksum()
         render json: {responseCode: 200, responseMessage: "Checksum generated successfully.",checksum_hash: @checksum_hash}
     end 
 
--- app/helpers/paytm_helper.rb
+++ app/helpers/paytm_helper.rb
@@ -2,7 +2,8 @@ module PaytmHelper
   require './lib/encryption_new_pg.rb'
   include EncryptionNewPG
   def generate_checksum
-    new_pg_checksum(@paramList,"dEYuOcfiwv038LF0").gsub("\n",'')
+    # new_pg_checksum(@paramList,"dEYuOcfiwv038LF0").gsub("\n",'')
+   new_pg_checksum(@paramList,"MwjkJxSWnJOIzdsY").gsub("\n",'')
   end
 
  end

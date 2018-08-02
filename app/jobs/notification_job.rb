1baa65e5a18d8a6c1b8f3d32e33960d2556515d3
aryan.arora <aryan.arora@mobiloitte.in>
2018-08-02 21:56:46 +0530

    Grocer gem added.

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -1,34 +1,45 @@
 class NotificationJob < ApplicationJob
   queue_as :default
   require 'notification_keys'
+  # def perform()
+  #   # sender = AdminUser.find_by_id(sender_id)
+  #   sender = "admin@example.com"
+  #   User.all.find_each(batch_size: 2000) do |receiver|
+  #   fcm = NotificationKeys::NotiKey.new.fcm_key
+  #   pusher = NotificationKeys::NotiKey.new.apns_key
+  #   receiver.devices.each do |device|
+  #       if device.device_type=="android"
+
+  #         registration_ids = ["#{device.device_token}"] if device.device_token
+  #         options = {"data":{
+  #            'message': ['badge': 1,'alert': "Notification",
+  #           "title": "New message:","body": "You got a new notification from #{sender}.", "click_action": "FCM_PLUGIN_ACTIVITY","sound": "default"]},"priority":"high"}
+  #         response = fcm.send_notification(registration_ids,options)
+
+  #         p "==========#{response.inspect}"
+  #       elsif (device.device_type == 'iOS')
+  #            notification = Grocer::Notification.new(
+  #                 :device_token => device.device_token.to_s,
+  #                 :alert => "You got a new notification from #{sender}.",
+  #                 :custom => { :notification_type => "UserNotification" },
+  #                 :badge => 1,
+  #                 :sound => "default"                    
+  #                 )
+  #           push = pusher.push(notification)
+  #           p "------------#{push.inspect}"
+  #       end
+  #     end
+  #   end
+  # end
+
+
   def perform()
-    # sender = AdminUser.find_by_id(sender_id)
-    sender = "admin@example.com"
-    User.all.find_each(batch_size: 2000) do |receiver|
-    fcm = NotificationKeys::NotiKey.new.fcm_key
-    pusher = NotificationKeys::NotiKey.new.apns_key
-    receiver.devices.each do |device|
-        if device.device_type=="android"
-
-          registration_ids = ["#{device.device_token}"] if device.device_token
-          options = {"data":{
-             'message': ['badge': 1,'alert': "Notification",
-            "title": "New message:","body": "You got a new notification from #{sender}.", "click_action": "FCM_PLUGIN_ACTIVITY","sound": "default"]},"priority":"high"}
-          response = fcm.send_notification(registration_ids,options)
-
-          p "==========#{response.inspect}"
-        elsif (device.device_type == 'iOS')
-             notification = Grocer::Notification.new(
-                  :device_token => device.device_token.to_s,
-                  :alert => "You got a new notification from #{sender}.",
-                  :custom => { :notification_type => "UserNotification" },
-                  :badge => 1,
-                  :sound => "default"                    
-                  )
-            push = pusher.push(notification)
-            p "------------#{push.inspect}"
-        end
-      end
-    end
+    notification = Grocer::Notification.new(
+                   :device_token => device.device_token.to_s,
+                   :alert => "You got a new notification from #{sender}.",
+                   :custom => { :notification_type => "UserNotification" },
+                   :badge => 1,
+                   :sound => "default"                    
+                   )
   end
 end

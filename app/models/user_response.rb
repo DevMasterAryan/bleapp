9c780eacc4c41b71b13954514d43aaa4ec3737bd
aryan.arora <aryan.arora@mobiloitte.in>
2018-08-03 21:04:52 +0530

    change in user responses model.

--- app/models/user_response.rb
+++ app/models/user_response.rb
@@ -6,6 +6,7 @@ class UserResponse
   field :rating, type: String
   field :help_id, type: Array, default: []
   field :feedback, type:String
+  field :site_id, type:Integer
   field :rating_status, type: Boolean, default: false
   # serialize :help_id
   belongs_to :user

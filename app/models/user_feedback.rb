5f62ce3d245a4a59d73481e97cfe8e2c70c73388
aryan.arora <aryan.arora@mobiloitte.in>
2018-08-03 21:17:39 +0530

    change in billing id

--- app/models/user_feedback.rb
+++ app/models/user_feedback.rb
@@ -4,6 +4,7 @@ class UserFeedback
   field :user_id, type: Integer
   field :site_id, type: Integer
   field :rating, type: Integer, default: 0
+  field :session_id, type: Integer
   field :rating_status, type: Boolean, default: false
   field :billing_id, type: Integer
   field :feedback, type: String

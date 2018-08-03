36025655afbf668e4753ab9a927d1ec4f96783b2
aryan.arora <aryan.arora@mobiloitte.in>
2018-08-03 21:15:43 +0530

    change in billing id

--- app/models/user_feedback.rb
+++ app/models/user_feedback.rb
@@ -1,8 +1,10 @@
 class UserFeedback
   include Mongoid::Document
-  field :help_id, type: String
+  field :help_id,  type: Array, default: []
   field :user_id, type: Integer
   field :site_id, type: Integer
   field :rating, type: Integer, default: 0
   field :rating_status, type: Boolean, default: false
+  field :billing_id, type: Integer
+  field :feedback, type: String
 end
--- app/models/user_response.rb
+++ app/models/user_response.rb
@@ -1,13 +1,10 @@
 class UserResponse
   include Mongoid::Document
-
   field :user_id, type: Integer
   field :billing_id, type: Integer
   field :rating, type: String
-  field :help_id, type: Array, default: []
+  field :help_id, type: String
   field :feedback, type:String
-  field :site_id, type:Integer
-  field :rating_status, type: Boolean, default: false
-  # serialize :help_id
+
   belongs_to :user
 end

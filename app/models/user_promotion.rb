6d34b17b2a62906e639c4ecb4ab88f5cbb7f00e8
aryan.arora <aryan.arora@mobiloitte.in>
2018-08-04 19:22:33 +0530

    change in user promotion

--- app/models/user_promotion.rb
+++ app/models/user_promotion.rb
@@ -11,6 +11,7 @@ class UserPromotion
   after_create :send_notification
    
   def send_notification
+  	p "=======#{self.user_id}======"
     user = User.find_by(id: self.user_id)
     promotion = Promotion.find_by(id: self.promotion_id)
     NotificationJob.perform_later(user, promotion) 	

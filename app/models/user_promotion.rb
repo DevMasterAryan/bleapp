8d0cbaa28dceaa18570c61e75f8a7e3093ac7274
aryan.arora <aryan.arora@mobiloitte.in>
2018-08-04 19:42:45 +0530

    otp failed resue

--- app/models/user_promotion.rb
+++ app/models/user_promotion.rb
@@ -11,9 +11,11 @@ class UserPromotion
   after_create :send_notification
    
   def send_notification
+  	binding.pry
   	p "=======#{self.user_id}======"
     user = User.find_by(id: self.user_id)
     promotion = Promotion.find_by(id: self.promotion_id)
-    NotificationJob.perform_later(user, promotion) 	
+    NotificationJob.perform_later(user, promotion&.promotion_count) 	
   end
 end
+

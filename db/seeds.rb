be2b35d90d1c086ad036be611da469c681468aad
aryan.arora <aryan.arora@mobiloitte.in>
2018-08-22 18:45:41 +0530

    change in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create(name: "SuperAdmin")
-Category.create(name: "Manager")
-Category.create(name: "Employee")
-Category.create(name: "Admin")
+require 'csv'    
+
+csv_text = File.read('./devices.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  Device.create!(row.to_hash)
+end

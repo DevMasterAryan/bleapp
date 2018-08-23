c62416a71295c8d64a0d7efb8738a3d10d71a29f
aryan.arora <aryan.arora@mobiloitte.in>
2018-08-23 17:15:56 +0530

    change in user feedback model

--- app/models/user_feedback.rb
+++ app/models/user_feedback.rb
@@ -1,12 +1,12 @@
 class UserFeedback
   include Mongoid::Document
   field :help_id,  type: Array, default: []
-  field :user_id, type: Integer
-  field :site_id, type: Integer
+  field :user_id, type: String
+  field :site_id, type: String
   field :rating, type: Integer, default: 0
-  field :session_id, type: Integer
+  field :session_id, type: String
   field :rating_status, type: Boolean, default: false
-  field :billing_id, type: Integer
+  field :billing_id, type: String
   field :feedback, type: String
 
   belongs_to :billing, optional: true

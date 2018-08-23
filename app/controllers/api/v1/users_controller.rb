3b13f7a04ec327f68f6da890380f6c59bb041c13
aryan.arora <aryan.arora@mobiloitte.in>
2018-08-23 12:39:58 +0530

    add credit key in credit api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -115,12 +115,12 @@ class Api::V1::UsersController < ApplicationController
 
       check_rated = @api_current_user.user_feedbacks.where(billing_id: last_billing.id) if last_billing.present? 
       if check_rated.present?
-         return render json: {responseCode: 200,responseMessage: "No billing found.", billing: [], promotion: @api_current_user.promotion_count} 
+         return render json: {responseCode: 200,responseMessage: "No billing found.", billing: [], promotion: @api_current_user.promotion_count, credit: @api_current_user.credit} 
       else
       	if last_billing.present?
-          return render json: {responseCode: 200, responseMessage: "Billing fetched successfully.", billing: [billing_id: last_billing.id.as_json["$oid"], site_name: last_billing&.session&.device&.site&.site_name], promotion: @api_current_user.promotion_count} 
+          return render json: {responseCode: 200, responseMessage: "Billing fetched successfully.", billing: [billing_id: last_billing.id.as_json["$oid"], site_name: last_billing&.session&.device&.site&.site_name], promotion: @api_current_user.promotion_count, credit: @api_current_user.credit} 
         else
-          return render json: {responseCode: 200,responseMessage: "No billing found.", billing: [], promotion: @api_current_user.promotion_count} 	
+          return render json: {responseCode: 200,responseMessage: "No billing found.", billing: [], promotion: @api_current_user.promotion_count, credit: @api_current_user.credit} 	
       	end
       end        
          

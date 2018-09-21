9b72d81a5100832451da0ed42f152c6894ea8876
aryan.arora <aryan.arora@mobiloitte.in>
2018-09-21 20:41:00 +0530

    changes in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 
 require 'csv'    
 
-csv_text = File.read('/devices.csv')
+csv_text = File.read('./public/devices.csv')
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   Device.create!(row.to_hash)
 end
 
 
-csv_text = File.read('/packages.csv')
+csv_text = File.read('./public/packages.csv')
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   Package.create!(row.to_hash)
 end
 
 
-csv_text = File.read('/static_contents.csv')
+csv_text = File.read('./public/static_contents.csv')
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   StaticContent.create!(row.to_hash)

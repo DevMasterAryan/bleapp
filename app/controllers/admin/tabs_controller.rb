65950869d25444b39883ba0b6937a1939bf0aa72
aryan.arora <aryan.arora@mobiloitte.in>
2018-09-24 14:55:05 +0530

    working on tab controller

--- app/controllers/admin/tabs_controller.rb
+++ app/controllers/admin/tabs_controller.rb
@@ -1,6 +1,10 @@
+require 'will_paginate/array'
+
 class Admin::TabsController < ApplicationController
+  layout 'admin_lte_2'
+  before_action :authenticate_admin_user
   def index
   	@category = Category.find_by(id: params[:category_id])
-  	@tabs = @category.tabs
+  	@tabs = @category.tabs.paginate(:page => params[:page], :per_page => 15)
   end
 end

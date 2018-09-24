be4202c1cdbddd0ab38a68e93fa3e84706a63ff2
Lalit Saini <lalit.saini@mobiloitte.in>
2018-09-24 11:36:52 +0530

    change in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
 
   namespace :admin do
-    resources :categories, only: [:new,:create, :edit, :update]
+    resources :categories #, only: [:new,:create, :edit, :update, :index]
     resources :accounts 
     resources :admin_users do
       collection{

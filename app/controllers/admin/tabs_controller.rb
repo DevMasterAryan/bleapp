721e6d3e95d90c37a2862417842cde54581e1cbf
aryan.arora <aryan.arora@mobiloitte.in>
2018-09-25 12:07:43 +0530

    working on tab creation(simple submission done)

--- app/controllers/admin/tabs_controller.rb
+++ app/controllers/admin/tabs_controller.rb
@@ -14,6 +14,15 @@ class Admin::TabsController < ApplicationController
   end
 
   def create
+    @category = Category.find_by(id: params[:category_id])
+    @tab = @category.tabs.new(name: params[:tab][:name], type: params[:tab][:type])
+    if @tab.save
+       @tab_tables = @tab.tab_tables.new(table_name: params[:table_name], table_columns: params[:tab][:column_names].to_unsafe_hash)
+       if @tab_tables.save
+          flash[:notice] =  "Tab created successfully"
+          redirect_to admin_category_tabs_path    
+       end
+    end
   	
   end
 
@@ -22,7 +31,7 @@ class Admin::TabsController < ApplicationController
   end
 
   def render_table
-     @column_names = params[:table].camelize.constantize.attribute_names - ["created_at", "updated_at"]
+     @column_names = params[:table].camelize.constantize.attribute_names - ["_id","updated_at"]
      @tab_type = params[:type]
   end
 
--- app/models/tab_table.rb
+++ app/models/tab_table.rb
@@ -2,7 +2,7 @@ class TabTable
   include Mongoid::Document
   field :tab_id, type: String
   field :table_name, type: String
-  field :table_columns, type: Array, default: []
-  belongs_to :tab, dependent: :destroy
+  field :table_columns, type: Hash, default: {}
+  belongs_to :tab
 
 end

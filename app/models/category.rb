5637c0b564f824f73a25531beab5cf8b9b383fde
Lalit Saini <lalit.saini@mobiloitte.in>
2018-09-25 11:32:55 +0530

    add validations to the input fields

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,5 +5,6 @@ class Category
   has_many :admin_users
 
   has_many :tabs, dependent: :destroy
+  validates :name, :presence => true, length: {minimum: 3, maximum: 30} ,format: { with: /[A-Za-z ]+/}
   
 end

bb8db9d9367a5d7d9dfc5cdadc0ce5fb7c411786
Lalit Saini <lalit.saini@mobiloitte.in>
2018-09-26 16:33:26 +0530

    view for table/form

--- app/controllers/admin/tabs_controller.rb
+++ app/controllers/admin/tabs_controller.rb
@@ -5,7 +5,7 @@ class Admin::TabsController < ApplicationController
   before_action :authenticate_admin_user
   def index
   	@category = Category.find_by(id: params[:category_id])
-  	@tabs = @category.tabs.paginate(:page => params[:page], :per_page => 15)
+  	@tabs = @category.tabs.reverse.paginate(:page => params[:page], :per_page => 15)
   end
 
   def new
@@ -14,7 +14,6 @@ class Admin::TabsController < ApplicationController
   end
 
   def create
-    binding.pry
     @category = Category.find_by(id: params[:category_id])
      @tab = @category.tabs.new(tab_params)
      if @tab.save
@@ -48,13 +47,17 @@ class Admin::TabsController < ApplicationController
       @lead_columns = ["site_name", "location_address", "address", "lat", "long", "account_id", "site_person", "site_mobile", "estimated_value", "sales_emp_id", "attachment", "quotation_id", "emp_remarks", "escalation_one", "escalation_two", "status_one", "status_two", "site_manager_id", "manager_id", "est_close_date", "s1_close_date", "s2_close_date", "manager_remark"] 
       @quotation_columns = ["site_id", "device_quantity", "hub_quantity", "one_time_retail_fee", "manager_approval", "qt_approval_date"] 
       @ddd_columns = [ "quotation_id", "site_id", "site_name", "site_location", "quotation_date", "device_id", "device_type", "attachment", "d3_id", "d3_creation_date", "wh_pickup_date"] 
-    
-     @tab_type = params[:type]
+      @tab_type = params[:type]
   end
 
   def render_table_table
-    @column_names = params[:table].camelize.constantize.attribute_names - ["updated_at"]
-    @tab_type = params[:type]
+      @account_columns = ["_id","created_at","name", "contact_person", "contact_email", "contact_mobile", "potential_site", "emp_id"]
+      @lead_columns = ["_id","created_at","site_name", "location_address", "address", "lat", "long", "account_id", "site_person", "site_mobile", "estimated_value", "sales_emp_id", "attachment", "quotation_id", "emp_remarks", "escalation_one", "escalation_two", "status_one", "status_two", "site_manager_id", "manager_id", "est_close_date", "s1_close_date", "s2_close_date", "manager_remark"] 
+      @quotation_columns = ["_id","created_at","site_id", "device_quantity", "hub_quantity", "one_time_retail_fee", "manager_approval", "qt_approval_date"] 
+      @ddd_columns = [ "_id","created_at","quotation_id", "site_id", "site_name", "site_location", "quotation_date", "device_id", "device_type", "attachment", "d3_id", "d3_creation_date", "wh_pickup_date"]  
+      @tab_type = params[:type]
+    # @column_names = params[:table].camelize.constantize.attribute_names - ["updated_at"]
+    # @tab_type = params[:type]
   end
 
   def checkbox_session

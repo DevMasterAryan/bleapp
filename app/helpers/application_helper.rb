b8ecac36238e1beff8068eec9f9571ef93a185a5
Lalit Saini <lalit.saini@mobiloitte.in>
2018-09-28 11:08:19 +0530

    code commit and pushed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,15 @@ module ApplicationHelper
   def logged_in?
     !current_user.nil?
   end	
-  
+
+
+   def super_admin_user?
+    return true if current_user.category.name=="SuperAdmin"
+   end 
+
+   def category_tabs
+     return current_user.category.tabs if current_user.category.tabs.present?
+   end
   
 
 end

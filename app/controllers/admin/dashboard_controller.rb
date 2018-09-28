cc0436a80745e603bd06501c5b5f26dadd2c547c
lalit.saini <lalit.saini@mobiloitte.in>
2018-09-28 19:08:44 +0530

    user based sidebar tab manangement

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,8 +2,12 @@ class Admin::DashboardController < ApplicationController
   layout 'admin_lte_2'
   before_action :authenticate_admin_user
   def home
-    # @admin_user = AdminUser.where(id: session[:user_id]["$oid"]).first
-
+     if params[:tab_id].present?
+        @tab  = Tab.find_by(id: params[:tab_id])
+        @tab_type = @tab.type
+        p "#{@tab_type}"
+       
+     end
   end
 
 

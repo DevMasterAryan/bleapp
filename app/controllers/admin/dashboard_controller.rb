ac42aa79d60f46582bfbf493ff15a0dee0703bc4
Lalit Saini <lalit.saini@mobiloitte.in>
2018-10-01 19:06:49 +0530

    display tab table view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,8 @@ class Admin::DashboardController < ApplicationController
   def home
      if params[:tab_id].present?
         @tab  = Tab.find_by(id: params[:tab_id])
+        @show_tabs = @tab.columns.keys 
+        @show = @tab.columns
         @tab_type = @tab.type
         p "#{@tab_type}"
         @table_name = @tab.columns.keys.first
@@ -15,23 +17,6 @@ class Admin::DashboardController < ApplicationController
      
      end
   end
-
-  def set_tab_value
-  	x = {"tab_values": params[:tab_value].as_json}
-  	key = params[:tab_value].as_json.keys.first
-  	value = params[:tab_value].as_json.values[0]
-  	Tab.find_by(id: params[:id]).value[:tab_values].delete(key) if Tab.find_by(id: params[:id]).value[:tab_values].keys.include?(key)
-  	# value = Tab.find_by(id: params[:id]).value.merge(x)
-  	abc = Tab.find_by(id: params[:id]).value
-  	abc[:tab_values][:"#{key}"] = value
-  	Tab.find_by(id: params[:id]).update(value: abc)
-  	redirect_to request.referer
-  end
-
-  def get_tab_value
-  	binding.pry
-  end
-
 
 
 end

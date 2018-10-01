3f3bfaccb092d3c6947c02a6de34d45b1dc18dcd
aryan.arora <aryan.arora@mobiloitte.in>
2018-10-01 19:11:49 +0530

    account creation Wip

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -1,16 +1,16 @@
 class Admin::AccountsController < ApplicationController
   layout 'admin_lte_2'
   before_action :authenticate_admin_user
-
-  def index
-  end
-
-  def new
-  	
-  end
-
   def create
-  	
+    @account  = Account.new(account_params)
+    if @account.save
+       flash[:notice] = "Account created successfully."
+       redirect_to request.referer 
+    else   
+       flash[:alert] = @account.errors.messages
+       redirect_to request.referer
+
+    end	
   end
 
   def edit
@@ -19,6 +19,12 @@ class Admin::AccountsController < ApplicationController
 
   def update
   	
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :contact_person, :contact_email, :contact_mobile, :potential_site, :emp_id)  
   end
   
 end
